496a481c51410915374b9f98618c181e8d668339
khrishchanovich <orannon@gmail.com>
2024-02-02 00:16:48 +0300

    Update script1.sh

--- Lab1/script1.sh
+++ Lab1/script1.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-ROWS=10
-COLUMNS=30
+rows=10
+columns=30
 
 main() {
     while true; do
-        local x=$((RANDOM % COLUMNS))
-        local y=$((RANDOM % ROWS))
+        local x=$((RANDOM % columns))
+        local y=$((RANDOM % rows))
         
         local time_str=$(date +%T)
         
         clear
         
         tput cup 0 0
-        echo "+$(printf '%*s' $COLUMNS | tr ' ' -)+"
+        echo "+$(printf '%*s' $columns | tr ' ' -)+"
         
-        for ((i=1; i<=ROWS; i++)); do
+        for ((i=1; i<=rows; i++)); do
             tput cup $i 0
             echo "|"
-            tput cup $i $((COLUMNS + 1))
+            tput cup $i $((columns + 1))
             echo "|"
         done
        
-        tput cup $ROWS 0
-        echo "+$(printf '%*s' $COLUMNS | tr ' ' -)+"
+        tput cup $rows 0
+        echo "+$(printf '%*s' $columns | tr ' ' -)+"
         
         tput cup $y $x
         echo "$time_str"
